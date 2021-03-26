235207d9ed6c86fb94eed7efdbb4320d59430d14
b0dhidharma <b0dhidharma@protonmail.com>
2021-03-26 17:07:42 -0300

    test: customizable basics wire framed

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -1,6 +1,7 @@
 export default {
   mainnet: {
     keep3r: '0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44',
+    keep3rGovernance: '0x2d407ddb06311396fe14d4b49da5f0471447d45c',
     yfi: {
       oracleBondedKeeper: '0xA8646cE5d983E996EbA22eb39e5956653ec63762',
       keeperV1Oracle: '0x73353801921417f465377c8d898c6f4c0270282c',
