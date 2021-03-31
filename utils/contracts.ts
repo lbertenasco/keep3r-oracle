0f0a5c21a4d85c8be78f48f4c247e6d0a08e3713
Luciano Bertenasco <lbertenasco@gmail.com>
2021-03-31 13:02:58 -0300

    feat: add yearn oracle factory address

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -3,6 +3,7 @@ export default {
     keep3r: '0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44',
     keep3rGovernance: '0x2d407ddb06311396fe14d4b49da5f0471447d45c',
     keeperV2OracleFactory: '0xaed599aadfee8e32cedb59db2b1120d33a7bacfd',
+    yearnKeeperV2OracleFactory: '0x25CC51298aE939fc1D34281FA595F3981830A365',
     keep3rV2OracleJob: '',
   },
 };
